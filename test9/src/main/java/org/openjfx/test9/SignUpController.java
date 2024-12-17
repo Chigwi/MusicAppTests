d0cb59c83a78dc7324db4eb724c7d9a9c7d6f0bb
Chigwi <carlos.a.gamboag@gmail.com>
2024-12-17 14:19:22 -0500

    fixed the error label on the sign up screen

--- test9/src/main/java/org/openjfx/test9/SignUpController.java
+++ test9/src/main/java/org/openjfx/test9/SignUpController.java
@@ -4,13 +4,17 @@
  */
 package org.openjfx.test9;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class SignUpController {
+public class SignUpController implements Initializable {
 
     @FXML
     private Button SignIn;
@@ -54,5 +58,12 @@ public class SignUpController {
     	}
     	return false;
     }
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		outError.setVisible(true);
+		outError.setText("Password requirements" + "\n" + "Make sure password has:" + "\n"
+    		+ "1. at least 1 capital leter" + "\n" + "2. at least 8 total characters" + "\n" + "3. at least 1 number");
+		
+	}
 
 }
