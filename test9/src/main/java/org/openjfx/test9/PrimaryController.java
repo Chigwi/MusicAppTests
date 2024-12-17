071e8888d1590d9e24236b452e91cfe3971a0085
Chigwi <carlos.a.gamboag@gmail.com>
2024-12-17 11:41:54 -0500

    added a checkbox to keep the user logged in

--- test9/src/main/java/org/openjfx/test9/PrimaryController.java
+++ test9/src/main/java/org/openjfx/test9/PrimaryController.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -35,6 +36,9 @@ public class PrimaryController {
 
     @FXML
     private Button primaryButton;
+    
+    @FXML
+    private CheckBox isLoggedIn;
     
     private SeralizationControler J = new SeralizationControler();
 	
@@ -64,6 +68,19 @@ public class PrimaryController {
     @FXML
     void switchToPrimary(ActionEvent event) {
 
+    }
+    //checks if the password is correct for the user name given 
+    private boolean checkPass (String userName, String Password) {
+    	try {
+    		if(J.deserializarUser(path, userName).getUsername().equals(userName)
+    			&& J.deserializarUser(path, userName).getPassword().equals(Password)) {
+    			return true;
+    		}
+    	}catch(Exception e) {
+    		OutError.setText("User doesn't exist");
+    		OutError.setVisible(true);
+    	}
+    	return false;
     }
 }
 
